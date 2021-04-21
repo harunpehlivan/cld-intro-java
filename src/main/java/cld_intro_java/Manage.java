72d697f3640de60916077490ede657557175e2e7
rebeccapeltz <rebecca.peltz@cloudinary.com>
2021-04-21 15:19:07 -0700

    move code into runner

--- src/main/java/cld_intro_java/Manage.java
+++ src/main/java/cld_intro_java/Manage.java
@@ -8,7 +8,7 @@ import com.cloudinary.utils.ObjectUtils;
 
 public class Manage {
 
-	@SuppressWarnings("unchecked")
+	@SuppressWarnings({ "unchecked", "unused" })
 	public static void runner(Cloudinary cloudinary) {
 
 		try {
@@ -16,8 +16,14 @@ public class Manage {
 			Map<String, Object> noOptions = ObjectUtils.emptyMap();
 
 			Api api = cloudinary.api();
+
+			// List all assets (default is 10)
 //			CourseUtils.jsonPrint(api.resources(noOptions));
 
+			// List up to 500 assets
+//			CourseUtils.jsonPrint(api.resources(ObjectUtils.asMap("max_results",500)));
+
+			// Search by prefix (public id "starts with")
 //			CourseUtils.jsonPrint(api.resources(ObjectUtils.asMap("type", "upload", "prefix", "sample")));
 
 			// Rename an asset, default overwrite is false
@@ -25,6 +31,40 @@ public class Manage {
 //					ObjectUtils.asMap("public_id", "cheesecake")));
 //			CourseUtils.jsonPrint(
 //					cloudinary.uploader().rename("cheesecake", "my_cheesecake", ObjectUtils.asMap("overwrite", true)));
+
+			// Rename an asset, default overwrite is false
+//			CourseUtils.jsonPrint(cloudinary.uploader().upload("./assets/cheesecake.jpg",
+//			ObjectUtils.asMap("public_id","cheesecake")));
+//			
+//			CourseUtils.jsonPrint(cloudinary.uploader().rename("cheesecake",
+//			"my_cheesecake",ObjectUtils.asMap("overwrite",true)));
+
+			// Remove an asset
+			// Upload API: destroy
+//			CourseUtils.jsonPrint(
+//					cloudinary.uploader().upload("./assets/lake.jpg", ObjectUtils.asMap("public_id", "lake")));
+//			CourseUtils.jsonPrint(cloudinary.uploader().destroy("lake", ObjectUtils.asMap("invalidate", true)));
+
+			// Admin API: delete_resources
+			// Upload 2 assets and them remove them
+//			CourseUtils
+//					.jsonPrint(cloudinary.uploader().upload("./assets/dog.jpg", ObjectUtils.asMap("public_id", "dog")));
+//			CourseUtils.jsonPrint(
+//					cloudinary.uploader().upload("./assets/lake.jpg", ObjectUtils.asMap("public_id", "lake")));
+//			CourseUtils.jsonPrint(
+//					api.deleteResources(Arrays.asList("dog", "lake"), ObjectUtils.asMap("invalidate", true)));
+
+			// Tag on Upload
+			// By string with comma-separated tags
+//			CourseUtils.jsonPrint(cloudinary.uploader().upload("./assets/blackberry.jpg",
+//					ObjectUtils.asMap("public_id", "blackberry", "tags", Arrays.asList("fruit", "berries"))));
+
+			// Tag after Upload
+//			CourseUtils.jsonPrint(
+//					cloudinary.uploader().upload("./assets/lake.jpg", ObjectUtils.asMap("public_id", "lake")));
+//			CourseUtils
+//					.jsonPrint(cloudinary.uploader().addTag("water", new String[] { "lake" }, ObjectUtils.emptyMap()));
+//			CourseUtils.jsonPrint(api.resourcesByTag("water", ObjectUtils.emptyMap()));
 
 		} catch (Exception e) {
 			System.out.println(e.getLocalizedMessage());
