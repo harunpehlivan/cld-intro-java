5dcfa6014eba804d720de8ecbaf03d34f3674551
rebeccapeltz <rebecca.peltz@cloudinary.com>
2021-04-22 11:36:20 -0700

    add uw;test code

--- src/main/java/cld_intro_java/NamedTransformations.java
+++ src/main/java/cld_intro_java/NamedTransformations.java
@@ -5,10 +5,44 @@ import com.cloudinary.Cloudinary;
 
 public class NamedTransformations {
 
+	@SuppressWarnings("unused")
 	public static void runner(Cloudinary cloudinary) {
 		try {
 
-		} catch (Error e) {
+			Api api = cloudinary.api();
+
+			// Create a simple named transformation from a string
+			// System.out.println(api.createTransformation("standard","w_150,h_150,c_thumb,g_auto",ObjectUtils.emptyMap()));
+
+			// Use named transformation standard
+			// System.out.println(cloudinary.url().transformation(new
+			// Transformation().named("standard")).imageTag("cheesecake.jpg"));
+
+			// Use named transform with f_auto
+			// System.out.println(cloudinary.url().transformation(new
+			// Transformation().named("standard").fetchFormat("auto")).imageTag("cheesecake.jpg"));
+
+			// Create and use complex transformation
+			// System.out.println(cloudinary.url().transformation(new
+			// Transformation().overlay(new
+			// Layer().publicId("logo")).effect("brightness:-21").x(-10).y(-200).width(304).radius("max")
+			// .chain().overlay(new
+			// TextLayer().fontFamily("Arial").fontSize(100).fontWeight("bold").text("Hello
+			// Jon")).opacity(70).x(-10).y(0).width(365)).imageTag("shirt_only.jpg"));
+
+			// Create a named transformation for the complex transformation
+			// System.out.println(api.createTransformation("complex", new
+			// Transformation().overlay(new
+			// Layer().publicId("logo")).effect("brightness:-21").x(-10).y(-200).width(304).radius("max")
+			// .chain().overlay(new
+			// TextLayer().fontFamily("Coustard").fontSize(100).fontWeight("bold").text("Hello
+			// Jon")).opacity(70).y(0).x(-10).width(365).generate(),ObjectUtils.emptyMap()));
+
+			// Use the complex named transform with f_auto
+			// System.out.println(cloudinary.url().transformation(new
+			// Transformation().named("complex").fetchFormat("auto")).imageTag("shirt_only.png"));
+
+		} catch (Exception e) {
 			System.out.println(e.getLocalizedMessage());
 		}
 	}
--- src/main/java/cld_intro_java/Presets.java
+++ src/main/java/cld_intro_java/Presets.java
@@ -6,8 +6,25 @@ import com.cloudinary.Cloudinary;
 public class Presets {
 	public static void runner(Cloudinary cloudinary) {
 		try {
+			Api api = cloudinary.api();
+			// Unsigned Presets
+			// Create an unsigned upload preset
+//			System.out.println(api.createUploadPreset(ObjectUtils.asMap("name", "unsigned-image", "unsigned", true,
+//					"tags", "unsigned", "allowed_formats", "jpg,png")));
 
-		} catch (Error e) {
+			// Use unsigned preset
+			// System.out.println(cloudinary.uploader().upload("./assets/logo.png",
+			// ObjectUtils.asMap("upload_preset","unsigned-image")));
+
+			// Create signed preset
+			// System.out.println(api.createUploadPreset(ObjectUtils.asMap("name",
+			// "signed-image", "unsigned", false, "tags" , "signed",
+			// "allowed_formats","jpg,png")));
+
+			// Use signed preset
+			// System.out.println(cloudinary.uploader().upload("./assets/lake.png",
+			// ObjectUtils.asMap("upload_preset","signed-image")));
+		} catch (Exception e) {
 			System.out.println(e.getLocalizedMessage());
 		}
 	}
