af1facafdb237f9ef93e2c4332b56e0634b185eb
rebeccapeltz <rebecca.peltz@cloudinary.com>
2022-09-08 08:26:59 -0700

    align utilities code with code in video

--- src/main/java/cld_intro_java/CloudinaryUtils.java
+++ src/main/java/cld_intro_java/CloudinaryUtils.java
@@ -11,9 +11,9 @@ public class CloudinaryUtils {
 	 * @param args
 	 */
 	public static void runner(Cloudinary cloudinary) {
-		System.out.println(cloudinary.url().generate("cheesecake"));
-		System.out.println(cloudinary.url().imageTag("cheesecake"));
-		System.out.println(cloudinary.url().resourceType("video").generate("earth"));
-		System.out.println(cloudinary.url().videoTag("earth"));
+		// System.out.println(cloudinary.url().generate("cheesecake"));
+		// System.out.println(cloudinary.url().resourceType("video").generate("earth"));
+		// System.out.println(cloudinary.url().imageTag("cheesecake"));
+		// System.out.println(cloudinary.url().videoTag("earth"));
 	}
 }
